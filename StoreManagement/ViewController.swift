2e23d34f9f4b45253804880d84cc8a51cfda52b7
GuillaumeLauzier <55703540+GuillaumeLauzier@users.noreply.github.com>
2019-10-30 19:45:41 +0100

    Initial Commit

---
+++ StoreManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreManagement
+//
+//  Created by ifage-user on 30.10.19.
+//  Copyright © 2019 ifage-user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
